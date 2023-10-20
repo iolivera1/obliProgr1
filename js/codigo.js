b80f5855b14b90d782969b5162d75dc1113c0b2a
Ivan Olivera <ivan.olivera@boronstudio.com>
2023-10-20 19:46:09 -0300

    ajustes

--- js/codigo.js
+++ js/codigo.js
@@ -1,104 +1,95 @@
-const WARNING_ICON = `<img src="img/warning.webp" height="20px" alt="Advertencia">`;
-const DENIED_ICON = `<img src="img/error.webp" height="20px" alt="Denegado">`;
-const APPROVED_ICON = `<img src="img/approved.webp" height="20px" alt="Aprobado">`;
-let sistema = new Sistema();
+let idUsuario = 0;
 
-
-habilitarNavegacion();
-
-
-function habilitarNavegacion() {
-  let botones = document.querySelectorAll(".btnNavegacion");
-
-  for (i = 0; i < botones.length; i++) {
-    botones[i].addEventListener("click", navegar);
-  }
-}
-
-function navegar() {
-  let boton = this;
-  let idDivMostrar = "#" + boton.getAttribute("data-mostrar");
-  mostrarPagina(idDivMostrar);
-}
-
-function mostrarPagina(id) {
-  let divsOcultar = document.querySelectorAll(".seccionPagina");
-
-  for (i = 0; i < divsOcultar.length; i++) {
-    divsOcultar[i].style.display = "none";
+class Sistema {
+  constructor() {
+    this.usuarios = [];
+    this.usuarioActual = null;
+    this.opcionesSelect = [
+      [
+        "c7.small (costo por alquiler: U$S 20 - costo por encendido: USD 2.50)",
+        "c7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
+        "c7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.00)",
+      ],
+      [
+        "r7.small (costo por alquiler: U$S 35 - costo por encendido: USD 4.00)",
+        "r7.medium (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
+        "r7.large (costo por alquiler: U$S 60 - costo por encendido: USD 7.00)",
+      ],
+      [
+        "i7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
+        "i7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
+      ],
+    ];
   }
 
-  document.querySelector(id).style.display = "block";
-}
-
-
-function crearUsuario()
-{
-  let nombre = document.querySelector("#txtNombreRegisto").value;
-  let apellido = document.querySelector("#txtApellidoRegisto").value;
-  let userName = document.querySelector("#txtUsernameRegistro").value;
-  let contrasenia = document.querySelector("#txtContraseniaRegistro").value;
-  let repeticionContrasenia = document.querySelector("#txtContraseniaRepeticionRegistro").value;
-  let nroTarjetaCredito = document.querySelector("#txtTarjetaCreditoNumero").value;
-  let cvc = document.querySelector("#txtCVC").value;
-
-  let msjError = validacionDeRegistro(nombre, apellido, userName, contrasenia, repeticionContrasenia)
-  if(msjError.length > 0)
-  {
-    document.querySelector("#pMsjErrorRegistroUsuario").innerHTML = msjError;
-    return;
-  }
-  document.querySelector("#pMsjErrorRegistroUsuario").innerHTML =``;
-  document.querySelector("#divRegistroFormaDePago").style.display = "block";
-  if(!formaDePagoEsValida(nroTarjetaCredito, cvc)) return;
-
-  document.querySelector("#pMsjErrorRegistroUsuario").innerHTML = `${APPROVED_ICON}`;
-  sistema.crearUsuario(nombre, apellido, userName, contrasenia);
-}
-
-
-function validacionDeRegistro(nombre, apellido, userName, contrasenia, repeticionContrasenia)
-{
-
-  let msjError = ``;
-  if(!nombre.length || !apellido.length)
-  {
-    msjError += `${DENIED_ICON} El nombre y apellido no pueden estar vacios<br>`;
-  }
-  if(!sistema.esNombreUsuarioValido(userName))
-  {
-    msjError += `${DENIED_ICON} El nombre de usuario debe tener entre 4 y 20 caracteres. No puede ser un numero <br>`;
-  }
-  if(!sistema.esContraseniaValida(contrasenia))
-  {
-    msjError += `${DENIED_ICON} La contrasenia debe tener al menos 5 caracteres y <br>
-    por lo menos una letra mayuscula, una minuscula y un numero <br>`;
-  }
-  if(!sistema.contraseniasCoinciden(contrasenia, repeticionContrasenia))
-  {
-    msjError = `${DENIED_ICON} Las contrasenias no coinciden <br>`;
-  }
-
-  return msjError;
-}
-
-
-function formaDePagoEsValida(nroTarjetaCredito, cvc)
-{
-  let msjError = ``;
-  if(!sistema.esTarjetaDeCreditoValida(nroTarjetaCredito, cvc))
-  {
-    msjError = `${WARNING_ICON} La forma de pago ingresada no es valida`;
-  }
-
-  document.querySelector("#pMsjErrorFormaPago").innerHTML = msjError;
-  if(msjError.length === 0)
-  {
+  crearUsuario(nombre, apellido, nombreUsuario, contrasenia) {
+    let usuario = new Usuario(nombre, apellido, nombreUsuario, contrasenia);
+    this.usuarios.push(usuario);
     return true;
   }
-  return false;
+
+  esNombreUsuarioValido(usuario) {
+    if (this.existeNombreUsuario(usuario)) return false;
+    if (usuario.length < 4 || usuario.length > 20 || Number(usuario)) {
+      return false;
+    }
+    return true;
+  }
+
+  existeNombreUsuario(usuario) {
+    let existe = false;
+    let i = 0;
+    while (i < this.usuarios.length && !existe) {
+      existe = this.usuarios[i].nombreUsuario === usuario;
+      i++;
+    }
+    return existe;
+  }
+
+  esContraseniaValida(contrasenia) {
+    if (contrasenia.length < 5) return false;
+    let tieneMayuscula = false;
+    let tieneMinuscula = false;
+    let tieneNumero = false;
+    let i = 0;
+    while (
+      i < contrasenia.length &&
+      (!tieneMayuscula || !tieneMinuscula || !tieneNumero)
+    ) {
+      tieneMayuscula =
+        contrasenia.charCodeAt(i) > 64 && contrasenia.charCodeAt(i) < 91
+          ? true
+          : tieneMayuscula;
+      tieneMinuscula =
+        contrasenia.charCodeAt(i) > 96 && contrasenia.charCodeAt(i) < 123
+          ? true
+          : tieneMinuscula;
+      tieneNumero = Number(contrasenia.charAt(i)) ? true : tieneNumero;
+      i++;
+    }
+    return tieneMayuscula && tieneMinuscula && tieneNumero;
+  }
+
+  contraseniasCoinciden(contrasenia, repetirContrasenia) {
+    return contrasenia === repetirContrasenia;
+  }
+
+  esTarjetaDeCreditoValida(nroTarjeta, cvc) {
+    if (!nroTarjeta.length || !cvc.length) {
+      return false;
+    }
+    return true; //TODO
+  }
 }
 
-//eventos fijos
+class Usuario {
+  constructor(nombre, apellido, nombreUsuario, contrasenia) {
+    this.id = idUsuario++;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.nombreUsuario = nombreUsuario;
+    this.contrasenia = contrasenia;
+    this.instancias = [];
+  }
+}
 
-document.querySelector("#btnCrearUsuario").addEventListener("click", crearUsuario);
