d8d0792d73ad47ca2ad5fb2c8215b9211d6dd170
martinooliveri <martinooliveri@gmail.com>
2023-10-22 09:35:48 -0300

    funcionalidad de login y logout completa

--- js/clases.js
+++ js/clases.js
@@ -1,60 +1,89 @@
 let idUsuario = 0;
 
 class Sistema {
+
   constructor() {
     this.usuarios = [];
     this.usuarioActual = null;
-    this.opcionesSelect = [
-      [
-        "c7.small (costo por alquiler: U$S 20 - costo por encendido: USD 2.50)",
-        "c7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
-        "c7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.00)",
-      ],
-      [
-        "r7.small (costo por alquiler: U$S 35 - costo por encendido: USD 4.00)",
-        "r7.medium (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
-        "r7.large (costo por alquiler: U$S 60 - costo por encendido: USD 7.00)",
-      ],
-      [
-        "i7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
-        "i7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
-      ],
-    ];
   }
 
+  /** En este punto todas las validaciones se tienen que haber realizado
+   *  Agrega un usaurio al array de usuarios
+   * 
+   * @param {String} nombre 
+   * @param {String} apellido 
+   * @param {String} nombreUsuario 
+   * @param {String} contrasenia 
+   */
   crearUsuario(nombre, apellido, nombreUsuario, contrasenia) {
     let usuario = new Usuario(nombre, apellido, nombreUsuario, contrasenia);
     this.usuarios.push(usuario);
+  }
+
+  /**Valida usuario y contrasenia, asigna al usuario actual si logra loguearlo
+   * 
+   * @param {String} nombreUsuario 
+   * @param {String} contrasenia 
+   * @returns true si el login es exitoso, false en otro caso
+   */
+  login(nombreUsuario, contrasenia)
+  {
+    if(!nombreUsuario || !contrasenia) return false;
+    let usuario = this.encontrarUsuario(nombreUsuario);
+    if(!usuario) return false;
+    if(usuario.contrasenia !== contrasenia) return false;
+    this.usuarioActual = usuario;
     return true;
   }
 
-  esNombreUsuarioValido(usuario) {
-    if (this.existeNombreUsuario(usuario)) return false;
-    if (usuario.length < 4 || usuario.length > 20 || Number(usuario)) {
+  /**
+   * Simplemente libera el usuario actual para ser coherente con lo que se vusaliza en pantalla
+   */
+  logout()
+  {
+    this.usuarioActual = null;
+  }
+
+  /**Un nombre de usuario es valido si tiene al menos 4 
+   * 
+   * @param {String} nombreUsuario 
+   * @returns true si el usuario es valido, false en otro caso
+   */
+  esNombreUsuarioValido(nombreUsuario) {
+    if (!this.encontrarUsuario(nombreUsuario)) return false;
+    if (nombreUsuario.length < 4 || nombreUsuario.length > 20 || Number(nombreUsuario)) {
       return false;
     }
     return true;
   }
 
-  existeNombreUsuario(usuario) {
-    let existe = false;
+  /**Busca un usuario por su nombre de usuario
+   * 
+   * @param {String} nombreUsuario 
+   * @returns {Usuario} un objeto de la clase Usuarios, si lo encuentra en usuarios[]; null en otro caso 
+   */
+  encontrarUsuario(nombreUsuario) {
+    let usuario = null;
     let i = 0;
-    while (i < this.usuarios.length && !existe) {
-      existe = this.usuarios[i].nombreUsuario === usuario;
+    while (i < this.usuarios.length && !usuario) {
+      usuario = this.usuarios[i].nombreUsuario === nombreUsuario ? this.usuarios[i] : null;
       i++;
     }
-    return existe;
+    return usuario;
   }
 
+  /** validacion de contrasenia ingresada en el registro de usuario
+   *
+   * @param {String} contrasenia 
+   * @returns true si la contrasenia ingresada es valida para asociar a un usuario, false en otro caso 
+   */
   esContraseniaValida(contrasenia) {
     if (contrasenia.length < 5) return false;
     let tieneMayuscula = false;
     let tieneMinuscula = false;
     let tieneNumero = false;
     let i = 0;
-    while (
-      i < contrasenia.length &&
-      (!tieneMayuscula || !tieneMinuscula || !tieneNumero)
+    while (i < contrasenia.length && (!tieneMayuscula || !tieneMinuscula || !tieneNumero)
     ) {
       tieneMayuscula =
         contrasenia.charCodeAt(i) > 64 && contrasenia.charCodeAt(i) < 91
@@ -70,29 +99,63 @@ class Sistema {
     return tieneMayuscula && tieneMinuscula && tieneNumero;
   }
 
-  contraseniasCoinciden(contrasenia, repetirContrasenia) {
-    return contrasenia === repetirContrasenia;
+  /** Validacion de contrasenia 
+   * 
+   * @param {String} contrasenia 
+   * @param {String} contraseniaRepeticion 
+   */
+  contraseniasCoinciden(contrasenia, contraseniaRepeticion) {
+    return contrasenia === contraseniaRepeticion;
   }
 
+  /** Valida tarjeta de credito usando el algoritmo de Luhn
+   * 
+   * @param {string} nroTarjeta 
+   * @param {Number} cvc 
+   * @returns true si la tarjeta ingresada es valida, false en otro caso
+   */
   esTarjetaDeCreditoValida(nroTarjeta, cvc) {
     
-    if (!nroTarjeta.length || !cvc.length) {
+    return true; //TODO
+    if (nroTarjeta.length !== 16 || cvc.length !== 3 ) {
       return false;
-    }
-
+    } 
+    let tarjetaConDuplicado = this.tarjetaConDuplicado(nroTarjeta);
+    sumarMayoresQueNueve(tarjetaConDuplicado);
 
 
     return true; 
   }
+
+  tarjetaConDuplicado(nroTarjeta)
+  {
+    let resultado = [];
+    for(let i = nroTarjeta.length - 2; i >= 0; i--)
+    {
+      resultado.push( nroTarjeta.charAt(i) % 2 === 0 ? nroTarjeta.charAt(i)*2 : nroTarjeta.charAt(i));
+    }
+    return resultado;
+  }
+
+  sumarMayoresQueNueve()
+  {
+
+  }
+
+  preCargarDatos()
+  {
+    this.usuarios.push(new Usuario("Martino", "Oliveri", "admin", "admin", true))
+  }
 }
 
 class Usuario {
-  constructor(nombre, apellido, nombreUsuario, contrasenia) {
+  constructor(nombre, apellido, nombreUsuario, contrasenia, esAdmin) {
     this.id = idUsuario++;
     this.nombre = nombre;
     this.apellido = apellido;
     this.nombreUsuario = nombreUsuario;
     this.contrasenia = contrasenia;
-    this.instancias = [];
-  }
+    this.instancias = esAdmin ? [] : null;
+    this.esAdmin = esAdmin;
+  }
 }
--- js/codigo.js
+++ js/codigo.js
@@ -1,7 +1,25 @@
 const WARNING_ICON = `<img src="img/warning.webp" height="20px" alt="Advertencia">`;
 const DENIED_ICON = `<img src="img/error.webp" height="20px" alt="Denegado">`;
 const APPROVED_ICON = `<img src="img/approved.webp" height="20px" alt="Aprobado">`;
+const TIPOS_INSTANCIA = [
+  [
+    "c7.small (costo por alquiler: U$S 20 - costo por encendido: USD 2.50)",
+    "c7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
+    "c7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.00)",
+  ],
+  [
+    "r7.small (costo por alquiler: U$S 35 - costo por encendido: USD 4.00)",
+    "r7.medium (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
+    "r7.large (costo por alquiler: U$S 60 - costo por encendido: USD 7.00)",
+  ],
+  [
+    "i7.medium (costo por alquiler: U$S 30 - costo por encendido: USD 3.50)",
+    "i7.large (costo por alquiler: U$S 50 - costo por encendido: USD 6.50)",
+  ],
+];
+
 let sistema = new Sistema();
+sistema.preCargarDatos();
 
 //eventos fijos
 
@@ -13,11 +31,19 @@ document
   .querySelector("#slcTipoInstanciaSeleccionada")
   .addEventListener("change", montarOpcionesInstancias);
 
-document.querySelector("#btnLogin").addEventListener('click', ingresarAlSistema)
+document.querySelector("#btnLogin").addEventListener('click', login)
+document.querySelector("#btnLogOut").addEventListener('click', logout)
 
+
+
+//fin eventos fijos
 
 habilitarNavegacion();
+mostrarPagina("#divLoginUsuario");
 
+/**
+ * Le da funcionalidad a los botones para navegar entre distintas partes de la pagina
+ */
 function habilitarNavegacion() {
   let botones = document.querySelectorAll(".btnNavegacion");
 
@@ -26,22 +52,31 @@ function habilitarNavegacion() {
   }
 }
 
+/**
+ * Carga la ID del div que se quiere mostrar, ID esta cargada como atributo del boton
+ */
 function navegar() {
   let boton = this;
   let idDivMostrar = "#" + boton.getAttribute("data-mostrar");
   mostrarPagina(idDivMostrar);
 }
 
+/** Oculta todos los div de una clase y muestra el ingresado por parametro
+ * 
+ * @param {String} id 
+ */
 function mostrarPagina(id) {
   let divsOcultar = document.querySelectorAll(".seccionPagina");
 
   for (i = 0; i < divsOcultar.length; i++) {
     divsOcultar[i].style.display = "none";
   }
-
   document.querySelector(id).style.display = "block";
 }
 
+/**
+ * Validacion del registro de usuario, chequea nombre de usuario, contrasenia
+ */
 function crearUsuario() {
   let nombre = document.querySelector("#txtNombreRegisto").value;
   let apellido = document.querySelector("#txtApellidoRegisto").value;
@@ -55,25 +90,24 @@ function crearUsuario() {
   ).value;
   let cvc = document.querySelector("#txtCVC").value;
 
-  if (
-    !datosDeRegistroSonValidos(
-      nombre,
-      apellido,
-      userName,
-      contrasenia,
-      repeticionContrasenia
-    )
-  )
-    return;
+  if (!datosDeRegistroSonValidos(nombre, apellido, userName, contrasenia, repeticionContrasenia)) return;
+
   document.querySelector("#divRegistroFormaDePago").style.display = "block";
   if (!formaDePagoEsValida(nroTarjetaCredito, cvc)) return;
 
-  document.querySelector(
-    "#pMsjErrorRegistroUsuario"
-  ).innerHTML = `${APPROVED_ICON}`;
+  document.querySelector("#pMsjRegistroUsuario").innerHTML = `${APPROVED_ICON} Usuario pendiente de activacion`;
   sistema.crearUsuario(nombre, apellido, userName, contrasenia);
 }
 
+/** Muetra mensaje de error por cada dato de registro que no cumpla sus condiciones de ingreso
+ * 
+ * @param {String} nombre 
+ * @param {String} apellido 
+ * @param {String} userName 
+ * @param {String} contrasenia 
+ * @param {String} repeticionContrasenia 
+ * @returns true si no hay mensajes de error para mostrar; false en otro caso
+ */
 function datosDeRegistroSonValidos(
   nombre,
   apellido,
@@ -83,13 +117,13 @@ function datosDeRegistroSonValidos(
 ) {
   let msjError = ``;
   if (!nombre.length || !apellido.length) {
-    msjError += `${DENIED_ICON} El nombre y apellido no pueden estar vacios<br>`;
+    msjError += `${DENIED_ICON} El nombre y el apellido no pueden estar vacios<br>`;
   }
   if (!sistema.esNombreUsuarioValido(userName)) {
     msjError += `${DENIED_ICON} El nombre de usuario debe tener entre 4 y 20 caracteres. No puede ser un numero <br>`;
   }
   if (!sistema.esContraseniaValida(contrasenia)) {
-    msjError += `${DENIED_ICON} La contrasenia debe tener al menos 5 caracteres y <br>
+    msjError += `${DENIED_ICON} La contrasenia debe tener al menos 5 caracteres y
     por lo menos una letra mayuscula, una minuscula y un numero <br>`;
   }
   if (!sistema.contraseniasCoinciden(contrasenia, repeticionContrasenia)) {
@@ -105,6 +139,12 @@ function datosDeRegistroSonValidos(
   return true;
 }
 
+/** Valida forma de pago ingresada en login
+ * 
+ * @param {Number} nroTarjetaCredito 
+ * @param {Number} cvc 
+ * @returns  true 
+ */
 function formaDePagoEsValida(nroTarjetaCredito, cvc) {
   let msjError = ``;
   if (!sistema.esTarjetaDeCreditoValida(nroTarjetaCredito, cvc)) {
@@ -112,21 +152,25 @@ function formaDePagoEsValida(nroTarjetaCredito, cvc) {
   }
 
   document.querySelector("#pMsjErrorFormaPago").innerHTML = msjError;
-  if (msjError.length === 0) {
-    return true;
+  
+  if (msjError.length > 0) {
+    return false;
   }
-  return false;
+
+  return true;
 }
 
+
+/**
+ * En la seccion alquiler, carga el segundo combo box dinamicamente usando el value del primer combo box
+ */
 function montarOpcionesInstancias() {
   let opcionSelect = Number(
     document.querySelector("#slcTipoInstanciaSeleccionada").value
   );
-  let divSelect = document.querySelector("#divTipoDeInstanciaC");
-  let select = document.querySelector("#slcTipoInstanciaC");
-
-
-  document.querySelector("#pErrorAlquiler").innerHTML = "";
+  let divSelect = document.querySelector("#divTipoDeInstancia");
+  let select = document.querySelector("#slcTipoInstancia");
+  document.querySelector("#pErrorAlquiler").innerHTML = ``;
 
   if (opcionSelect == -1) {
     document.querySelector(
@@ -134,13 +178,13 @@ function montarOpcionesInstancias() {
     ).innerHTML = `${DENIED_ICON} Seleccione una opción`;
     divSelect.style.display = "none";
   } else {
-    divSelect.style.display = "block";
     cargarSelect(select, opcionSelect);
+    divSelect.style.display = "block";
   }
 }
 
 function cargarSelect(select, valor) {
-  let opciones = sistema.opcionesSelect[valor];
+  let opciones = TIPOS_INSTANCIA[valor];
   while (select.options.length > 0) {
     select.remove(0);
   }
@@ -158,8 +202,27 @@ function cargarSelect(select, valor) {
   }
 }
 
-function ingresarAlSistema() {
+/**
+ * Lleva a cabo el login de usuario, muestra un mensaje de error si no logra hacer el login
+ * si logra el login muestra la pantalla de alquiler
+ */
+function login() {
   let usuario = document.querySelector("#txtUsernameoLogin").value
   let contraseña = document.querySelector("#txtContraseniaLogin").value
+  document.querySelector("#pErrorLogin").innerHTML = ``;
+  if(sistema.login(usuario, contraseña) === false)
+  {
+    document.querySelector("#pErrorLogin").innerHTML = `${DENIED_ICON} La combinacion de usuario y contrasenha no son correctas`;
+  }
+  else
+  {
+    mostrarPagina("#divAlquilerDeInstancias");
+  }
+}
+
+function logout()
+{
+  sistema.logout();
+  mostrarPagina("#divLoginUsuario");
 
 }
