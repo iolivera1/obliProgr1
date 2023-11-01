d645a7769bd6726b44a34256fdebf4f8dba55730
Ivan Olivera <ivan.olivera@boronstudio.com>
2023-11-01 10:49:11 -0300

    cambios en clase

--- js/clases.js
+++ js/clases.js
@@ -1,40 +1,47 @@
+const ESTADO_PENDIENTE = "pendiente";
+const ESTADO_ACTIVO = "activo";
+const ESTADO_BLOQUEADO = "bloqueado";
+const OPTIMIZADA_COMPUTO = "c7";
+const OPTIMIZADA_ALMACENAMIENTO = "i7";
+const OPTIMIZADA_MEMORIA = "r7";
+const TAMANIO_CHICO = "small";
+const TAMANIO_MEDIO = "medium";
+const TAMANIO_GRANDE = "large";
+let idTipoInstancia = 0;
 let idUsuario = 0;
-let idInstancia = 0;
-class Sistema 
-{
+let idAlquiler = 0;
 
-  constructor() 
-  {
+class Sistema {
+  constructor() {
     this.usuarios = [];
     this.usuarioActual = null;
+    this.tiposDeInstancias = [];
   }
 
   /** En este punto todas las validaciones se tienen que haber realizado
    *  Agrega un usuario al array de usuarios
-   * 
-   * @param {String} nombre 
-   * @param {String} apellido 
-   * @param {String} nombreUsuario 
-   * @param {String} contrasenia 
+   *
+   * @param {String} nombre
+   * @param {String} apellido
+   * @param {String} nombreUsuario
+   * @param {String} contrasenia
    */
-  crearUsuario(nombre, apellido, nombreUsuario, contrasenia) 
-  {
+  crearUsuario(nombre, apellido, nombreUsuario, contrasenia) {
     let usuario = new Usuario(nombre, apellido, nombreUsuario, contrasenia);
     this.usuarios.push(usuario);
   }
 
   /**Valida usuario y contrasenia, asigna al usuario actual si logra loguearlo
-   * 
-   * @param {String} nombreUsuario 
-   * @param {String} contrasenia 
+   *
+   * @param {String} nombreUsuario
+   * @param {String} contrasenia
    * @returns true si el login es exitoso, false en otro caso
    */
-  login(nombreUsuario, contrasenia)
-  {
-    if(!nombreUsuario || !contrasenia) return false;
+  login(nombreUsuario, contrasenia) {
+    if (!nombreUsuario || !contrasenia) return false;
     let usuario = this.encontrarUsuario(nombreUsuario);
-    if(!usuario) return false;
-    if(usuario.contrasenia !== contrasenia) return false;
+    if (!usuario) return false;
+    if (usuario.contrasenia !== contrasenia) return false;
     this.usuarioActual = usuario;
     return true;
   }
@@ -42,42 +49,43 @@ class Sistema
   /**
    * Simplemente libera el usuario actual para ser coherente con lo que se vusaliza en pantalla
    */
-  logout()
-  {
+  logout() {
     this.usuarioActual = null;
   }
 
-  /**Un nombre de usuario es valido si tiene al menos 4 caracteres y no mas de 20 
-   * 
-   * @param {String} nombreUsuario 
+  /**Un nombre de usuario es valido si tiene al menos 4 caracteres y no mas de 20
+   *
+   * @param {String} nombreUsuario
    * @returns true si el usuario es valido, false en otro caso
    */
-  esNombreUsuarioValido(nombreUsuario) 
-  {
-    if (nombreUsuario.length < 4 || nombreUsuario.length > 20 || Number(nombreUsuario)) 
-    {
+  esNombreUsuarioValido(nombreUsuario) {
+    if (
+      nombreUsuario.length < 4 ||
+      nombreUsuario.length > 20 ||
+      Number(nombreUsuario)
+    ) {
       return false;
     }
     return true;
   }
 
-  existeNombreDeUsuario(nombreUsuario)
-  {
-    return (this.encontrarUsuario(nombreUsuario) !== null);
+  existeNombreDeUsuario(nombreUsuario) {
+    return this.encontrarUsuario(nombreUsuario) !== null;
   }
 
   /**Busca un usuario por su nombre de usuario
-   * 
-   * @param {String} nombreUsuario 
-   * @returns {Usuario} un objeto de la clase Usuarios, si lo encuentra en usuarios[]; null en otro caso 
+   *
+   * @param {String} nombreUsuario
+   * @returns {Usuario} un objeto de la clase Usuarios, si lo encuentra en usuarios[]; null en otro caso
    */
-  encontrarUsuario(nombreUsuario) 
-  {
+  encontrarUsuario(nombreUsuario) {
     let usuario = null;
     let i = 0;
-    while (i < this.usuarios.length && !usuario) 
-    {
-      usuario = this.usuarios[i].nombreUsuario === nombreUsuario ? this.usuarios[i] : null;
+    while (i < this.usuarios.length && !usuario) {
+      usuario =
+        this.usuarios[i].nombreUsuario === nombreUsuario
+          ? this.usuarios[i]
+          : null;
       i++;
     }
     return usuario;
@@ -85,18 +93,19 @@ class Sistema
 
   /** validacion de contrasenia ingresada en el registro de usuario
    *
-   * @param {String} contrasenia 
-   * @returns true si la contrasenia ingresada es valida para asociar a un usuario, false en otro caso 
+   * @param {String} contrasenia
+   * @returns true si la contrasenia ingresada es valida para asociar a un usuario, false en otro caso
    */
-  esContraseniaValida(contrasenia) 
-  {
+  esContraseniaValida(contrasenia) {
     if (contrasenia.length < 5) return false;
     let tieneMayuscula = false;
     let tieneMinuscula = false;
     let tieneNumero = false;
     let i = 0;
-    while (i < contrasenia.length && (!tieneMayuscula || !tieneMinuscula || !tieneNumero)) 
-    {
+    while (
+      i < contrasenia.length &&
+      (!tieneMayuscula || !tieneMinuscula || !tieneNumero)
+    ) {
       tieneMayuscula =
         contrasenia.charCodeAt(i) > 64 && contrasenia.charCodeAt(i) < 91
           ? true
@@ -111,104 +120,152 @@ class Sistema
     return tieneMayuscula && tieneMinuscula && tieneNumero;
   }
 
-  /** Validacion de contrasenia 
-   * 
-   * @param {String} contrasenia 
-   * @param {String} contraseniaRepeticion 
+  /** Validacion de contrasenia
+   *
+   * @param {String} contrasenia
+   * @param {String} contraseniaRepeticion
    */
-  contraseniasCoinciden(contrasenia, contraseniaRepeticion) 
-  {
+  contraseniasCoinciden(contrasenia, contraseniaRepeticion) {
     return contrasenia === contraseniaRepeticion;
   }
 
   /** Valida tarjeta de credito usando el algoritmo de Luhn;
    *  no funciona todavia jeje
-   * @param {string} nroTarjeta 
-   * @param {Number} cvc 
+   * @param {string} nroTarjeta
+   * @param {Number} cvc
    * @returns true si la tarjeta ingresada es valida, false en otro caso
    */
-  esTarjetaDeCreditoValida(nroTarjeta, cvc) 
-  {
+  esTarjetaDeCreditoValida(nroTarjeta, cvc) {
     const LARGO_TARJETA = 16;
-    if (nroTarjeta.length !== LARGO_TARJETA || cvc.length !== 3 ) return false;
+    if (nroTarjeta.length !== LARGO_TARJETA || cvc.length !== 3) return false;
     let tarjetaConDuplicado = this.tarjetaConDuplicado(nroTarjeta);
     let resultado = 0;
-    for(let i = 0; i < LARGO_TARJETA; i++)
-    {
+    for (let i = 0; i < LARGO_TARJETA; i++) {
       resultado += this.sumarMayoresQueNueve(tarjetaConDuplicado[i]);
     }
 
-    return (resultado*9 % 10) === nroTarjeta % 10;
+    return (resultado * 9) % 10 === nroTarjeta % 10;
   }
 
-  tarjetaConDuplicado(nroTarjeta)
-  {
+  tarjetaConDuplicado(nroTarjeta) {
     let resultado = [];
-    for(let i = nroTarjeta.length - 2; i >= 0; i--)
-    {
+    for (let i = nroTarjeta.length - 2; i >= 0; i--) {
       let numero = Number(nroTarjeta.charAt(i));
-      resultado.push((numero % 2 === 0) ? numero*2 : numero);
+      resultado.push(numero % 2 === 0 ? numero * 2 : numero);
     }
     return resultado;
   }
 
-  sumarMayoresQueNueve(numero)
-  {
-    if(numero > 9)
-    {
-      return 1 + numero % 10;
+  sumarMayoresQueNueve(numero) {
+    if (numero > 9) {
+      return 1 + (numero % 10);
     }
     return numero;
   }
 
-  otorgarInstanciaAUsuario(usuario, precioPorEncendido, precioPorAlquiler)
-  {
-    usuario.instancias.push(new Instancia(precioPorEncendido, precioPorAlquiler));
+  otorgarInstanciaAUsuario(usuario, costoPorEncendido, costoPorAlquiler) {
+    //TODO: revisar el new de instancia.
+    usuario.instancias.push(new Instancia(costoPorEncendido, costoPorAlquiler));
   }
 
-  preCargarDatos()
-  {
+  preCargarDatos() {
     this.usuarios.push(new Usuario("Martino", "Oliveri", "admin", "admin"));
     this.usuarios[0].esAdmin = true;
   }
+
+  filtrarTiposDeInstanciasPorOptimizacion(tipo) {
+    let resultado = [];
+    for (i = 0; i < this.tiposDeInstancias.length; i++) {
+      if (tipo === this.tiposDeInstancias.tipo) {
+        resultado.push(this.tiposDeInstancias[i]);
+      }
+    }
+    return resultado;
+  }
 }
 
-class Usuario 
-{
+class Usuario {
   /**Un usuario es unico (ID) y puede alquilar instancias, por default no son admin
-   * 
-   * @param {String} nombre 
-   * @param {String} apellido 
-   * @param {String} nombreUsuario 
-   * @param {String} contrasenia 
+   *
+   * @param {String} nombre
+   * @param {String} apellido
+   * @param {String} nombreUsuario
+   * @param {String} contrasenia
+   * @param {Number} tarjeta
+   * @param {Number} cvc
+   * @param {Boolean} esAdmin
+   * @param {Array} alquileres
    */
-  constructor(nombre, apellido, nombreUsuario, contrasenia) 
-  {
+  constructor(
+    nombre,
+    apellido,
+    nombreUsuario,
+    contrasenia,
+    tarjeta,
+    cvc,
+    esAdmin,
+    alquieres
+  ) {
     this.id = idUsuario++;
     this.nombre = nombre;
     this.apellido = apellido;
     this.nombreUsuario = nombreUsuario;
     this.contrasenia = contrasenia;
+    this.tarjeta = tarjeta;
+    this.cvc = cvc;
     this.instancias = [];
-    this.esAdmin = false;
+    this.esAdmin = esAdmin;
+    this.alquieres = alquieres;
+    this.estado = ESTADO_PENDIENTE;
   }
 }
 
-class Instancia
-{
+class TipoInstancia {
   /**
-   * 
-   * @param {Number} precioPorEncendido 
-   * @param {Number} precioPorAlquiler 
+   *
+   * @param {Number} costoPorEncendido
+   * @param {Number} costoPorAlquiler
+   * @param {String} tamanio
+   * @param {String} tipo
+   * @param {Number} stockInicial
    */
-  constructor(tipoInstancia, precioPorEncendido, precioPorAlquiler)
-  {
-    this.id = idInstancia++;
-    this.encendida = true;
-    this.tipoInstancia = tipoInstancia;
-    this.precioPorEncendido = precioPorEncendido;
-    this.precioPorAlquiler = precioPorAlquiler;
+  constructor(
+    costoPorEncendido,
+    costoPorAlquiler,
+    tamanio,
+    tipo,
+    stockInicial
+  ) {
+    this.id = "INSTANCE_ID_" + idTipoInstancia++;
+    this.costoPorEncendido = costoPorEncendido;
+    this.costoPorAlquiler = costoPorAlquiler;
+    this.tamanio = tamanio;
+    this.tipo = tipo;
+    this.stockInicial = stockInicial;
+  }
+}
+
+class Alquiler {
+  /**
+   *
+   * @param {TipoInstancia} TipoInstancia
+   * @param {Number} idUsuario
+   */
+  constructor(idUsuario, instancia) {
+    this.idUsuario = idUsuario;
+    this.idAlquiler = idAlquiler++;
+    this.instancia = instancia;
+    this.encendidos = 1;
+    this.encendido = true;
   }
 
+  encenderMaquina() {
+    this.encendido = true;
+    this.encendidos++;
+  }
 
+  apagarMaquina() {
+    this.encendido = false;
+  }
 }
+
